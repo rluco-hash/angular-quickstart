e49a333ad8a507a8bd376ecd00d6c0a54da61401
Renato Luco Ode <rluco@queplan.cl>
2026-01-19 15:26:57 -0300

    [ADD] se agrega iframe prueba front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['../demo-styling.css'],
 })
 export class AppComponent {
   title = 'angular-quickstart';
+  iframeUrl: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://demo-lirmi.queplan.cl',
+    );
+  }
 }
