b029e625ffb8dc34df6cb951abe8a6048f63f900
Renato Luco Ode <rluco@queplan.cl>
2026-08-17 12:12:39 -0400

    [CHG] prueba de netlify funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,5 @@ export class AppComponent {
   title = 'angular-quickstart';
   iframeUrl: { default?: SafeResourceUrl; community?: SafeResourceUrl } = {};
 
-  constructor(private sanitizer: DomSanitizer) {
-    this.iframeUrl.default = this.sanitizer.bypassSecurityTrustResourceUrl(
-      'https://widget.segurosdigital.cl/?key=6ced0b9db5d63a319d903a2ea83649ed3158bddd4909a6ea',
-    );
-
-    this.iframeUrl.community = this.sanitizer.bypassSecurityTrustResourceUrl(
-      'https://widget.segurosdigital.cl/?key=6ced0b9db5d63a319d903a2ea83649ed3158bddd4909a6ea&template=community',
-    );
-  }
+  constructor(private sanitizer: DomSanitizer) {}
 }
